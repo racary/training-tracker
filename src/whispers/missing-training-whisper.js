4dc92dab423b492ce3adcc7804139891a3b79df8
Ryan Cary <rcary@Ryans-MacBook-Pro.local>
2021-09-16 14:54:32 -0400

    added to designs for manager flow 1. current - new markdown with username followed by list of incomplete courses 2. commented out - new whisper for each employee with list of incomplete courses logic for creating the managers direct reports incomplete courses

--- src/whispers/missing-training-whisper.js
+++ src/whispers/missing-training-whisper.js
@@ -1,7 +1,47 @@
 import { user, whisper } from '@oliveai/ldk';
 import { Buffer } from 'buffer';
 
+const { Box, ListPair, Markdown } = whisper.WhisperComponentType;
+const { Left } = whisper.JustifyContent;
+const { Vertical } = whisper.Direction;
+
 const res = [
+  {
+    enrollment_id: 1425526,
+    content_type: 'Uploaded Policy',
+    module_name: 'Acceptable Use Policy',
+    user: {
+      id: 796742,
+      first_name: 'Sarah',
+      last_name: 'Thomas',
+      email: 'test@test.com',
+    },
+    campaign_name: 'OWASP 10',
+    enrollment_date: '2019-04-02T15:02:38.000Z',
+    start_date: '2019-04-02T15:02:38.000Z',
+    completion_date: '',
+    status: 'Passed',
+    time_spent: 2340,
+    policy_acknowledged: false,
+  },
+  {
+    enrollment_id: 1425526,
+    content_type: 'Uploaded Policy',
+    module_name: 'Acceptable Use Policy',
+    user: {
+      id: 796742,
+      first_name: 'Sarah',
+      last_name: 'Thomas',
+      email: 'test@test.com',
+    },
+    campaign_name: 'Compliance Training',
+    enrollment_date: '2019-04-02T15:02:38.000Z',
+    start_date: '2019-04-02T15:02:38.000Z',
+    completion_date: '',
+    status: 'Passed',
+    time_spent: 2340,
+    policy_acknowledged: false,
+  },
   {
     enrollment_id: 1425526,
     content_type: 'Uploaded Policy',
@@ -58,18 +98,32 @@ const res = [
   },
 ];
 
-const createComponent = (email) => {
-  const trainingsByEmail = res
-    .filter((r) => r.user.email === email && !r.completion_date)
-    .map((r) => ({ body: r.campaign_name, type: whisper.WhisperComponentType.Markdown }));
+const findIncompleteTrainingsByEmail = (email) => {
+  const incompleteTrainigs = res
+  .filter((r) => r.user.email === email && !r.completion_date)
+  .map(training => training.campaign_name);
+  return incompleteTrainigs;
+}
 
-  if (!trainingsByEmail.length) {
-    trainingsByEmail.push({
-      body: 'No missing trainings!!',
+const findDirectReportsMissingTraining = reports => {
+  return reports.map(report => {
+    return {
+      name: `${report.firstName} ${report.lastName}`,
+      missingTraining: findIncompleteTrainingsByEmail(report.email)
+    }
+  });
+}
+
+const createComponentMyIncompleteTrainings = (myEmail) => {
+  const myMissingTrainings = findIncompleteTrainingsByEmail(myEmail).map((r) => ({ body: r, type: Markdown }))
+
+  if (!myMissingTrainings.length) {
+    myMissingTrainings.push({
+      body: 'No missing trainings for me!!',
       type: whisper.WhisperComponentType.Markdown,
     });
   }
-  return trainingsByEmail;
+  return myMissingTrainings;
 };
 
 const decodeJWTToken = (token) => {
@@ -85,6 +139,37 @@ const decodeJWTToken = (token) => {
   }
 };
 
+const createDirectReportsWhisper = async (directReports) => {
+  
+  if (directReports) {
+    const directReportsMissingTraining = findDirectReportsMissingTraining(directReports);
+
+    if (directReportsMissingTraining.length) {
+      const components = directReportsMissingTraining.map((drMissingTraining) => {
+        return {
+          type: Box,
+          justifyContent: Left,
+          direction: Vertical,
+          children: [{type: Markdown, body: `${drMissingTraining.name} is missing following trainings: ${drMissingTraining.missingTraining.join(', ')}`}] //drMissingTraining.missingTraining.map((mt) => ({ type: ListPair, label: drMissingTraining.name, value: mt, copyable: true, style: whisper.Urgency.None }))
+        }
+      });
+
+      await whisper.create({
+        label: 'Direct Reports Incomplete Training',
+        onClose: () => console.log('Closed Direct Reports Training Whisper'),
+        components
+      });
+
+      // directReportsMissingTraining.forEach(async (directReportMissingTraining) => {
+      //   await whisper.create({
+      //     label: `${directReportMissingTraining.name} is Missing Following Trainings`,
+      //     components: directReportMissingTraining.missingTraining.map((mt) => ({ type: Markdown, body: mt }))
+      //   });
+      // });
+    }
+  }
+}
+
 export const trainingWhisper = async (email) => {
   let userEmail = email;
   if (!userEmail) {
@@ -93,8 +178,10 @@ export const trainingWhisper = async (email) => {
   }
   console.log(`current user email ==> ${userEmail}`);
   await whisper.create({
-    label: 'Missing Trainings',
+    label: 'My Missing Trainings',
     onClose: () => console.log('Closed Training Whisper'),
-    components: createComponent(userEmail),
+    components: createComponentMyIncompleteTrainings(userEmail),
   });
+
+  await createDirectReportsWhisper([{email: 'test@test.com', firstName: 'T', lastName: 'C'}])
 };
